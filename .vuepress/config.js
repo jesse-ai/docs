4f6765f82a0a2e51ba2c787863756c6105448e86
Sully Fischer <fischersully@gmail.com>
2019-02-07 18:57:23 +0330

    update config

---
+++ .vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    title: 'Jesse AI',
+    description: 'An advanced trading framework for cryptocurrency markets.',
+    themeConfig: {
+        docsRepo: 'jesse-ai/docs',
+        docsBranch: 'master',
+        editLinks: true,
+        editLinkText: 'Help us improve this page!', 
+
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Documentation', link: '/docs/' },
+            { text: 'Roadmap', link: 'https://github.com/jesse-ai/jesse/projects/1' },
+            { text: 'Github', link: 'http://github.com/jesse-ai/jesse' }
+        ],
+        sidebar: [
+            '/',
+            ['/docs/', 'Introduction'],
+            ['/docs/installation', 'Installation'],
+            // ['/docs/installation', 'Installation']
+        ]
+    }
+};
