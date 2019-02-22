f87c486601ae3e6b9a1de2cb13e8aa5eff8a9a11
Sully Fischer <fischersully@gmail.com>
2019-02-22 20:50:31 +0330

    add Google Analytics

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,24 +1,43 @@
 module.exports = {
     title: 'Jesse AI',
     description: 'An advanced trading framework for cryptocurrency markets.',
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-135004813-1'
+            }
+        ]
+    ],
     themeConfig: {
         docsRepo: 'jesse-ai/docs',
         docsBranch: 'master',
         editLinks: true,
-        editLinkText: 'Help us improve this page!', 
+        editLinkText: 'Help us improve this page!',
 
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Documentation', link: '/docs/' },
-            { text: 'Roadmap', link: 'https://github.com/jesse-ai/jesse/projects/1' },
-            { text: 'Github', link: 'http://github.com/jesse-ai/jesse' }
+        nav: [{
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'Documentation',
+                link: '/docs/'
+            },
+            {
+                text: 'Roadmap',
+                link: 'https://github.com/jesse-ai/jesse/projects/1'
+            },
+            {
+                text: 'Github',
+                link: 'http://github.com/jesse-ai/jesse'
+            }
         ],
         sidebar: [
             '/',
             ['/docs/', 'Introduction'],
             ['/docs/installation', 'Installation'],
             ['/docs/directory-structure', 'Directory Structure'],
-            ['/docs/indicators', 'Indicators'], 
+            ['/docs/indicators', 'Indicators'],
             ['/docs/helpers', 'Helpers']
         ]
     }
