8dbeb513697e50d0d5b92d1b22fafbac30c9443f
morteza-koohgard <morteza73.koohgard@gmail.com>
2021-09-26 11:46:47 +0330

    add new theme

---
+++ docs/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+    <template #page-bottom />
+  </Layout>
+</template>
+
+<script>
+import Layout from "@vuepress/theme-default/lib/client/layouts/404.vue";
+
+export default {
+    components: {
+        Layout
+    }
+};
+</script>
---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <template #page-bottom />
+  </Layout>
+</template>
+
+<script>
+import Layout from "@vuepress/theme-default/lib/client/layouts/Layout.vue";
+
+export default {
+    components: {
+        Layout
+    }
+};
+</script>
+
+<style >
+@font-face {
+  font-family: ProximaNova-Regular;
+  src: url('../font/webFonts/ProximaNovaRegular/font.woff');
+}
+body {
+  font-family: ProximaNova-Regular;
+}
+</style>
