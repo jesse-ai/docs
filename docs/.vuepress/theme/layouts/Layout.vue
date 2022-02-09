74b101164b544456adcff70d2685070678ca92c0
Saleh Mir <algo@hey.com>
2022-02-09 19:17:05 +0100

    add sponsored links

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,24 +1,35 @@
 <template>
   <Layout>
-    <template #page-bottom />
+    <template v-slot:sidebar-top>
+      <SidebarAd />
+    </template>
+
+    <template #page-bottom>
+      <FooterAd />
+    </template>
   </Layout>
 </template>
 
 <script>
 import Layout from "@vuepress/theme-default/lib/client/layouts/Layout.vue";
+import SidebarAd from "./SidebarAd.vue";
+import FooterAd from "./FooterAd.vue";
 
 export default {
-    components: {
-        Layout
-    }
+  components: {
+    Layout,
+    SidebarAd,
+    FooterAd
+  }
 };
 </script>
 
-<style >
+<style>
 @font-face {
   font-family: ProximaNova-Regular;
   src: url('../font/webFonts/ProximaNovaRegular/font.woff');
 }
+
 body {
   font-family: ProximaNova-Regular;
 }
